36de7cf21d73623bb298ef1920d6f7afcc610b37
Aakeefah Jones <aakeefahj@gmail.com>
2023-11-20 14:27:01 +0200

    send help

--- code.js
+++ code.js
@@ -1,2 +1,14 @@
 const btnSubmit = document.querySelector('[data-submit]')
 btnSubmit.addEventListener('click', birthday)
+
+let input1 = document.getElementById('firstName');
+let input2 = document.getElementById('dateOfBirth');
+
+let today = new Date();
+
+birthday.setFullYear(today.getFullYear());
+if (today > birthday) {
+  birthday.setFullYear(today.getFullYear() + 1);
+}
+
+Math.floor((birthday - today) / (1000*60*60*24))
